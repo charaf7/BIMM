5fc12a663b0ae47828bdc69cd987278cb14ef74d
Unknown <a.tojo.antonio@gmail.com>
2018-04-02 23:23:18 +0200

    implement graph axe 1

--- js/script.js
+++ js/script.js
@@ -1,37 +1,72 @@
-var ctx = document.getElementById('axe1Chart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'polarArea',
+$('document').ready(function () {
 
-    // The data for our dataset
-    data: {
-        labels: ["Offre"],
-        datasets: [{
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [20],
-        }]
-    },
+    var buildSkillArray = function () {
+        var skillArray = [];
+        $('.question-item').each(function( index ) {
+            var value = $(this).find('input[type=radio]:checked').val();
+            skillArray.push({
+                value: value ? parseInt(value) : 0,
+                coeff: 0
+            });
 
-    // Configuration options go here
-    options: {
-        scale: {
-            ticks: {
-                max: 100,
-                stepSize: 10
-            }
+        });
+        return skillArray;
+    };
+
+    var sumSkills = function (skillArray) {
+        var sum = 0
+        skillArray.forEach(function (skillValue) {
+            sum += skillValue;
+        })
+        return sum;
+    }
+
+    var skills = buildSkillArray();
+    var total = sumSkills(skills);
+    console.log(skills);
+    console.log(total);
+
+    
+    $('#axeForm input').on('change', function() {
+
+    });
+
+    var ctx = document.getElementById('axe1Chart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'polarArea',
+
+        // The data for our dataset
+        data: {
+            labels: ["Offre"],
+            datasets: [{
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [20],
+            }]
         },
-        tooltips: {
-            callbacks: {
-                label: function(tooltipItems, data) {
-                    return data.labels + ': ' + tooltipItems.yLabel + '%';
+
+        // Configuration options go here
+        options: {
+            scale: {
+                ticks: {
+                    max: 100,
+                    stepSize: 10
+                }
+            },
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItems, data) {
+                        return data.labels + ': ' + tooltipItems.yLabel + '%';
+                    }
                 }
             }
         }
-    }
-});
+    });
 
-setTimeout(function(){
-    chart.data.datasets[0].data[0] = 60;
-    chart.update();
-}, 3000);
+    setTimeout(function(){
+        chart.data.datasets[0].data[0] = 60;
+        chart.update();
+    }, 3000);
+
+});
--- src/view/axe1.php
+++ src/view/axe1.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Menu</title>
+    <title>Offre</title>
     <link rel="stylesheet" href="skin/dashboard.css" />
 
 </head>
@@ -24,17 +24,17 @@
 
 <main>
     <div class="block-container">
-        <form method="post" action="<?php echo $this->router->saveAxe(1); ?>">
+        <form id="axeForm" method="post" action="<?php echo $this->router->saveAxe(1); ?>">
             <div class="questions">
             <?php foreach ($axe1 as $q){?>
-                <div class="q1">
+                <div class="question-item">
                     <p> <?php echo $q->getQuestion();?></p>
-                    <input type="radio" name="<?php echo $q->getQuestionId();?>" value="0" <?php if($q->getValue() == 0) echo "checked" ?>>0
-                    <input type="radio" name="<?php echo $q->getQuestionId();?>" value="1" <?php if($q->getValue() == 1) echo "checked" ?>>1
-                    <input type="radio" name="<?php echo $q->getQuestionId();?>" value="2" <?php if($q->getValue() == 2) echo "checked" ?>>2
-                    <input type="radio" name="<?php echo $q->getQuestionId();?>" value="3" <?php if($q->getValue() == 3) echo "checked" ?>>3
-                    <input type="radio" name="<?php echo $q->getQuestionId();?>" value="4" <?php if($q->getValue() == 4) echo "checked" ?>>4
-                    <input type="radio" name="<?php echo $q->getQuestionId();?>" value="5" <?php if($q->getValue() == 5) echo "checked" ?>>5
+                    <input type="radio" name="<?php echo $q->getQuestionId();?>" data-coeff=<?php echo $q->getCoeff();?> value="0" <?php if($q->getValue() == 0) echo "checked" ?>>0
+                    <input type="radio" name="<?php echo $q->getQuestionId();?>" data-coeff=<?php echo $q->getCoeff();?> value="1" <?php if($q->getValue() == 1) echo "checked" ?>>1
+                    <input type="radio" name="<?php echo $q->getQuestionId();?>" data-coeff=<?php echo $q->getCoeff();?> value="2" <?php if($q->getValue() == 2) echo "checked" ?>>2
+                    <input type="radio" name="<?php echo $q->getQuestionId();?>" data-coeff=<?php echo $q->getCoeff();?> value="3" <?php if($q->getValue() == 3) echo "checked" ?>>3
+                    <input type="radio" name="<?php echo $q->getQuestionId();?>" data-coeff=<?php echo $q->getCoeff();?> value="4" <?php if($q->getValue() == 4) echo "checked" ?>>4
+                    <input type="radio" name="<?php echo $q->getQuestionId();?>" data-coeff=<?php echo $q->getCoeff();?> value="5" <?php if($q->getValue() == 5) echo "checked" ?>>5
                 </div>
             <?php }?>
             </div>
@@ -56,6 +56,7 @@
     </div>
 </main>
 
+<script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/chart.min.js"></script>
 <script src="js/script.js"></script>
 </body>
